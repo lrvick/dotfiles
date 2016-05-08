52b1e966bff9758d416883f6bc494f2e74e5a8dc
Lance R. Vick <lance@lrvick.net>
2016-05-07 21:25:12 -0700

    break out system specific screen/keyboard config

---
+++ .host_config/mayim/screenlayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 3440x1440 --pos 1080x0 --rotate normal --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI4 --off
