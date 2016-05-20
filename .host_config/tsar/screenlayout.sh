60bf9da9d499582ddc8e9e81465118681f3db766
Lance R. Vick <lance@lrvick.net>
2016-05-20 15:35:26 -0700

    add tsar screenlayout

---
+++ .host_config/tsar/screenlayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --off --output DP-4 --mode 1920x1200 --pos 3720x488 --rotate left --output DP-3 --mode 1920x1080 --pos 1800x852 --rotate normal --output DP-2 --mode 2880x1800 --pos 0x0 --rotate left --output DP-1 --off --output DP-0 --off
