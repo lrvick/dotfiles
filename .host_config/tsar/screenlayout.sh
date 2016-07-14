ef7e337b14540f71b077f13879ce5519406eb7ce
Lance R. Vick <lance@lrvick.net>
2016-07-13 19:43:57 -0700

    update work screen layout

--- .host_config/tsar/screenlayout.sh
+++ .host_config/tsar/screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --off --output DP-4 --mode 1920x1200 --pos 3720x488 --rotate left --output DP-3 --mode 1920x1080 --pos 1800x852 --rotate normal --output DP-2 --mode 2880x1800 --pos 0x0 --rotate left --output DP-1 --off --output DP-0 --off
+xrandr --output HDMI-0 --mode 1920x1080 --pos 4080x0 --rotate normal --output DP-4 --mode 1920x1080 --pos 6000x0 --rotate left --output DP-3 --primary --mode 1920x1200 --pos 2880x0 --rotate left --output DP-2 --mode 2880x1800 --pos 0x0 --rotate normal --output DP-1 --off --output DP-0 --off
