aecfd9a0b265784c383fb1d919f169fabcaceb93
Lance R. Vick <lance@lrvick.net>
2017-03-03 09:10:21 -0800

    triple screen home layout for dafuk

--- .host_config/dafuk/screenlayout.sh
+++ .host_config/dafuk/screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output HDMI1 --mode 2048x1152 --pos 0x0 --rotate left --output eDP1 --off --output DP1 --off --output HDMI2 --off --output DP1-1-2 --mode 3440x1440 --pos 1152x0 --rotate normal --output DP1-1-1 --off --output DP1-2 --off --output DP1-1 --off --output DP1-1-1-8 --mode 2560x1440 --pos 4592x0 --rotate right --output DP1-1-1-1 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output DP1-1-8 --mode 2560x1440 --pos 4880x0 --rotate right --output HDMI1 --mode 3440x1440 --pos 1440x0 --rotate normal --output DP1-1-1 --off --output HDMI2 --off --output DP1-1 --off --output DP1-8 --mode 2560x1440 --pos 0x0 --rotate left
